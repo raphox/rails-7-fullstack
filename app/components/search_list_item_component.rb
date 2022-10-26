80a0e82a34ccb2b459041ff03520514c8f2cf54e
Raphael Araújo <raphox.araujo@gmail.com>
2022-10-26 08:57:36 -0300

    Improved code format

--- app/components/search_list_item_component.rb
+++ app/components/search_list_item_component.rb
@@ -15,20 +15,20 @@ class SearchListItemComponent < ViewComponent::Base
   def call
     html_options = {
       aria: { label: "Edit this #{@item.model_name.singular}" },
-      data: { turbo_frame: "#{@item.model_name.singular}_form" } ,
+      data: { turbo_frame: "#{@item.model_name.singular}_form" },
       class: class_names(
-        "flex items-center justify-between w-full p-2 rounded", {
-          "bg-indigo-100": active?
+        'flex items-center justify-between w-full p-2 rounded', {
+          'bg-indigo-100': active?,
         }
-      )
+      ),
     }
 
     turbo_frame_tag dom_id(@item, 'list') do
-      content_tag :li, class: "-mx-2" do
+      content_tag :li, class: '-mx-2' do
         link_to @item, html_options do
-          content_tag :div, class: "flex items-center truncate" do
-            content_tag :div, class: "truncate" do
-              content_tag :div, @label, class: "text-sm font-medium text-slate-800"
+          content_tag :div, class: 'flex items-center truncate' do
+            content_tag :div, class: 'truncate' do
+              content_tag :div, @label, class: 'text-sm font-medium text-slate-800'
             end
           end
         end
