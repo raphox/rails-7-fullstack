7cc1b0b6f8f42c87868db8089de0010b708a0674
Raphael Araújo <raphox.araujo@gmail.com>
2022-10-27 00:10:28 -0300

    Inserted stimulus features

--- app/components/search_list_item_component.rb
+++ app/components/search_list_item_component.rb
@@ -9,29 +9,40 @@ class SearchListItemComponent < ViewComponent::Base
   end
 
   def active?
-    current_page?(kit_product_path(@item))
+    @item.persisted? && current_page?(kit_product_path(@item))
   end
 
   def call
     html_options = {
       aria: { label: "Edit this #{@item.model_name.singular}" },
-      data: { turbo_frame: "#{@item.model_name.singular}_form" },
+      data: {
+        turbo_frame: "#{@item.model_name.singular}_form",
+        action: 'click->search-list#selectItem',
+        search_list_target: 'item',
+      },
       class: class_names(
         'flex items-center justify-between w-full p-2 rounded', {
           'bg-indigo-100': active?,
+          'text-slate-300': @item.new_record?,
         }
       ),
     }
 
     turbo_frame_tag dom_id(@item, 'list') do
       content_tag :li, class: '-mx-2' do
-        link_to @item, html_options do
-          content_tag :div, class: 'flex items-center truncate' do
-            content_tag :div, class: 'truncate' do
-              content_tag :div, @label, class: 'text-sm font-medium text-slate-800'
-            end
-          end
+        link_to_if(@item.persisted?, item_tag, @item, html_options) do
+          content_tag :span, item_tag, html_options.except(:data)
         end
+      end
+    end
+  end
+
+  private
+
+  def item_tag
+    content_tag :div, class: 'flex items-center truncate' do
+      content_tag :div, class: 'truncate' do
+        content_tag :div, @label, class: 'text-sm font-medium'
       end
     end
   end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,3 +3,6 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
+
+import SearchListController from "./search_list_controller"
+application.register("search-list", SearchListController)
