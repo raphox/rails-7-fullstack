3663634de27821db150371ae79a1ded7ed3cc072
Raphael Araújo <raphox.araujo@gmail.com>
2022-11-24 14:19:40 -0300

    Using new hosting

--- db/migrate/20221022131756_create_kit_components.rb
+++ db/migrate/20221022131756_create_kit_components.rb
@@ -3,7 +3,7 @@
 class CreateKitComponents < ActiveRecord::Migration[7.0]
   def change
     create_table(:kit_components) do |t|
-      t.references(:product, null: false, foreign_key: true)
+      t.references(:kit_product, null: false, foreign_key: true)
       t.string(:name)
 
       t.timestamps
